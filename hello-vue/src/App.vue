9b0f33fcec7972a785c5f57bf959b683ff941379
ChiKim22 <wlsgh2505@gmail.com>
2021-07-22 19:32:30 +0900

    Vue.TodoList

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>My Todo App!</h1>
+        <TodoList></TodoList>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name:'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1{
+  text-align: center;
+}
+</style>
---
+++ hello-vue/src/components/TodoList.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <BaseInputText
+            v-model="newTodoText"
+            placeholder="New todo"
+            @keydown.enter="addTodo"
+            />
+    <ul v-if="todos.length">
+        <TodoListItem
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+            @remove="removeTodo"
+            @check="checkTodo"
+        />
+    </ul>
+
+    <p v-else>
+        Nothing left in the list. Add a new todo in input plz.
+    </p>    
+</div>
+</template>
+
+
+<script>
+import BaseInputText  from './BaseInputText.vue'
+import TodoListItem from './TodoListItem.vue'
+
+let nextTodoId = 1
+
+export default {
+    components: {
+        BaseInputText, TodoListItem
+    },
+    data(){
+        return {
+            newTodoText: '',
+            todos:[
+                {
+                    id: nextTodoId++,
+                    text: 'Example'
+                }
+            ]
+        }
+    },
+    methods:{
+        addTodo() {
+            const trimmeddText = this.newTodoText.trim()
+            if (trimmeddText) {
+                this.todos.push({
+                id: nextTodoId++,
+                text: trimmeddText
+                })
+                this.newTodoText = ''
+            }
+        },
+        removeTodo(idToRemove) {
+            this.todos = this.todos.filter(todo => {
+                return todo.id !==  idToRemove
+            })
+        },
+        checkTodo(){
+            const date = new Date()
+
+            this.date = date.getDate()
+
+            const todos = date
+
+            return todos == checkTodo
+
+        }
+    }
+}
+
+</script>
