34ae7f726b25466d1705dfb3520b848ee98b81c7
ChiKim22 <wlsgh2505@gmail.com>
2021-08-02 19:04:43 +0900

    Vue +Vuex

---
+++ my-cafe/vue.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    devServer:{
+        proxy:{
+            '/api':{
+                target:'https://jsonplaceholder.typicode.com',
+                changeOrgin:true,
+                pathRewrite:{
+                    '^/api':''
+                }
+            }
+        }
+    }
+}
